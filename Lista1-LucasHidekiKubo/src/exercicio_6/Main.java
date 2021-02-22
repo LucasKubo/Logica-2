6db0da9d79d115e4585f05abd15034d17254ba61
LucasKubo <lucaskbk2@gmail.com>
2021-02-22 19:36:36 -0300

    ADDED: Correção no exercício 6

--- Lista1-LucasHidekiKubo/src/exercicio_6/Main.java
+++ Lista1-LucasHidekiKubo/src/exercicio_6/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Tempo t1 = new Tempo(17,32,00);
-		t1.imprimeTempo();
-		System.out.println(t1.somaTempo(10, 10));
+		Tempo t1 = new Tempo(17,11,20);
+		Tempo t2 = new Tempo(19,32,21);
+		
+		t1.somaTempo(t2);
+		System.out.println(t1);
+		
+		t1.substraiTempo(t2);
+		System.out.println(t1);
 	}
 
 }
--- Lista1-LucasHidekiKubo/src/exercicio_6/Tempo.java
+++ Lista1-LucasHidekiKubo/src/exercicio_6/Tempo.java
@@ -45,12 +45,35 @@ public class Tempo {
 		System.out.println(horas+":"+minutos+":"+segundos);
 	}
 	
-	public int substraiTempo(int tempo1, int tempo2) {
-		return tempo1-tempo2;
+	public void substraiTempo(Tempo tempo2) {
+		//Transformando os tempos em segundos
+		int tempo1EmSec = this.horas*3600 + this.minutos*60 + this.segundos;
+		int tempo2EmSec = tempo2.horas*3600 + tempo2.minutos*60 + tempo2.segundos; 
+		int resultado = (tempo1EmSec>tempo2EmSec)? tempo1EmSec - tempo2EmSec : tempo2EmSec - tempo1EmSec;
+		
+		if(resultado != 0) {  //Verificando se os tempos não são iguais
+			//Atribuindo o resultado da substração dos tempos em horas, minutos e segundos.
+			this.horas = resultado / 3600;
+			int aux = resultado % 3600;
+			this.minutos = aux / 60;
+			this.segundos = aux % 60;
+		}
 	}
 	
-	public int somaTempo(int tempo1, int tempo2) {
-		return tempo1+tempo2;
+	public void somaTempo(Tempo tempo2) {
+		//Transformando os tempos em segundos
+		int tempo1EmSec = this.horas*3600 + this.minutos*60 + this.segundos;
+		int tempo2EmSec = tempo2.horas*3600 + tempo2.minutos*60 + tempo2.segundos; 
+		int resultado = tempo1EmSec + tempo2EmSec;
+		
+		//Atribuindo o resultado da soma dos tempos em horas, minutos e segundos.
+		this.horas = resultado / 3600;
+		int aux = resultado % 3600;
+		this.minutos = aux / 60;
+		this.segundos = aux % 60;
 	}
 
+	public String toString() {
+		return "Tempo: "+ horas + ":"+ minutos+ ":"+ segundos;
+	}
 }
